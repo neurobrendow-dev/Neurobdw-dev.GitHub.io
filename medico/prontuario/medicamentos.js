1ee5b3cdc83123efe511d0d488d1142b140b5722
neurobrendow-dev <neuro.brendow@gmail.com>
2026-05-11 20:40:26 -0300

    Refactor Vitamina B12 entries in medicamentos.js

--- medico/prontuario/medicamentos.js
+++ medico/prontuario/medicamentos.js
@@ -947,10 +947,8 @@ window.MEDICATIONS = [
     '1.000 UI comprimido', '2.000 UI comprimido', '7.000 UI comprimido',
     '50.000 UI comprimido', '200 UI/gota solução oral'
   ]},
-  { name: 'Vitamina B12 (cianocobalamina)', aliases: ['Cobavital'], presentations: [
-    '5.000 mcg/mL solução injetável', '5000 mcg comprimido'
-  ]},
-  { name: 'Vitamina B12 (Mecobalamina)', aliases: ['Dozemast', 'Mecobe'], presentations: ['1000 mcg comprimido sublingual']},
+  { name: 'Vitamina B12 (cianocobalamina)', aliases: ['Cobavital'], presentations: ['5.000 mcg/mL solução injetável', '5000 mcg comprimido'] },
+  { name: 'Vitamina B12 (Mecobalamina)', aliases: ['Dozemast', 'Mecobe'], presentations: ['1000 mcg cp sublingual'] },
   { name: 'Ácido fólico', aliases: ['Endofolin', 'Folacin'], presentations: ['5 mg comprimido', '0,2 mg/mL solução oral'] },
   { name: 'Vitamina B1 (tiamina)', aliases: ['Benerva'], presentations: ['100 mg comprimido', '300 mg comprimido'] },
   { name: 'Vitamina B6 (piridoxina)', aliases: ['Vitabe'], presentations: ['50 mg comprimido'] },
